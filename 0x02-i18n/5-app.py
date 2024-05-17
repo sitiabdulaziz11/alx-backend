9a89e06998e4ac5455ac23418ccac4743c597d5e
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-17 23:36:07 +0300

    all tasks

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,65 +1,62 @@
 #!/usr/bin/env python3
-""" Python script that starts a Flask web application """
-
-from flask import Flask, render_template, request
+"""
+Module 5-app
+"""
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
-app = Flask(__name__)
-app.config['LANGUAGES'] = ['en', 'fr']
 
-babel = Babel(app)
-
-
-class Config:
-    """ Class for configuration """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
 users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
 
-def get_user():
-    """ Mock user log in """
-    user = request.args.get('login_as')
-    if user is not None and (user.isnumeric() and
-                             int(user) in users.keys()):
-        return users[int(user)]
-    return None
+class Config(object):
+    """i18n configuration"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-@app.before_request
-def before_request():
-    """ Execute get_user before all functions """
-    user = get_user()
-    if user is not None:
-        g.user = user
-    else:
-        g.user = None
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """displays a basic hello world message"""
+    return render_template("5-index.html")
 
 
 @babel.localeselector
 def get_locale():
-    """ Function that determining the user's preferred language and local"""
-    local = request.args.get('locale')
-    if local and local in app.config['LANGUAGES']:
-        return local
+    """Gets best fmatch locale according to request"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-def index():
-    """ Function that returns the index.html template """
-    locale_user = get_locale()
-    return render_template('5-index.html', user_local=locale_user)
+def get_user():
+    """returns a given user"""
+    user_id = request.args.get('login_as')
+
+    if user_id is None:
+        return None
+
+    return users.get(int(user_id))
+
+
+@app.before_request
+def before_request():
+    """sets a user object to flask.g"""
+    user = get_user()
+    g.user = user
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port="5000")
